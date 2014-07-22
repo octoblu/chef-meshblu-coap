d082f14446a19c70c74e3695de2d597bf73c1fde
Jade Meskill <jade@integrumtech.com>
2014-07-21 17:15:38 -0700

    simplify

--- metadata.rb
+++
@@ -1,8 +0,0 @@
-name             'chef-octoblu'
-maintainer       'Octoblu'
-maintainer_email 'hello@octoblu.com'
-license          'All rights reserved'
-description      'Installs/Configures chef-octoblu'
-long_description 'Installs/Configures chef-octoblu'
-version          '0.1.0'
-
